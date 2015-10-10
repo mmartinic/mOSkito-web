737a51cdb03623e16d001e191fdfa13dd8f27ab0
Mihovil Rister <mrister@rhapsody.com>
2015-10-10 23:45:49 +0200

    add favicon module.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -14,6 +15,7 @@ app.set('views', path.join(process.env.PWD, 'views'));
 app.set('view engine', 'jade');
 app.set('port', (process.env.PORT || 3000));
 
+app.use(favicon(process.env.PWD + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
